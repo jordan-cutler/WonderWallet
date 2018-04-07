896110779455cccff94d92ca22889ea903e48025
Jordan Cutler <jdc219@lehigh.edu>
2018-04-06 23:37:48 -0400

    added redux and now console.log creating an account

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
 
 class App extends Component {
+
   render() {
+    console.log(this.props.web3.eth.accounts.create());
+    console.log(this.props.web3.eth.accounts.create('hello'));
     return (
       <div className="App">
         <header className="App-header">
@@ -18,4 +22,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    web3: state.web3
+  }
+};
+
+export default connect(mapStateToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,8 @@
-const initialState = {
+import Web3 from 'web3';
+let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
+const initialState = {
+  web3: web3
 };
 
 const reducer = (state = initialState, action) => {
