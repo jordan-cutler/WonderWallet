aa7d6d337f3f2ffb35a0d5ed7554db60f083e96f
Jordan Cutler <jdc219@lehigh.edu>
2018-04-07 02:01:18 -0400

    Fixed up reducer to use proper url and have non working version of keystore upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
-
+const VITALIK_ADDRESS = '0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B';
 class App extends Component {
 
   render() {
-    console.log(this.props.web3.eth.accounts.create());
+    const acc = this.props.web3.eth.accounts.create();
+    console.log(acc);
     console.log(this.props.web3.eth.accounts.create('hello'));
+    const vitalikAccountBalance = this.props.web3.eth.getBalance(VITALIK_ADDRESS);
+    vitalikAccountBalance.then(console.log);
+    const password = "testabc";
+    const keystore = this.props.web3.eth.accounts.encrypt(acc.privateKey, password);
+    console.log(keystore);
+    const decrypted = this.props.web3.eth.accounts.decrypt(keystore, password);
+    console.log('decrypted',decrypted);
     return (
       <div className="App">
         <header className="App-header">
@@ -17,8 +25,33 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href={new Blob([keystore, {type : 'application/json'}])}
+           download={this.generateFileName(acc.address)}
+        >
+          Download keystore
+        </a>
+        <input type="file" name="keystoreInput" onChange={(event) => this.fileUploaded(event)} />
+        <h1>Sep</h1>
+        <input type="file" name="keystoreInput" onChange={(event) => this.imageUploaded(event)} />
       </div>
     );
+  }
+
+  generateFileName(publicKey) {
+    const today = new Date();
+    return `UTC--${today}-${publicKey}.json`;
+  }
+
+  imageUploaded(event) {
+    console.log(event.target.files[0]);
+  }
+
+  fileUploaded(event) {
+    const file = event.target.files[0];
+    console.log(file);
+    const keystore = this.props.web3.eth.accounts.decrypt(file, "testabc");
+    console.log(keystore);
+    console.log('file uploaded');
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3';
-let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+let web3 = new Web3("https://rinkeby.infura.io/4VagvCAdkEkPEFAJw8LU");
 
 const initialState = {
   web3: web3
