4ccd66cd21a80727d6c309560d009ab007b7d885
John Cunningham <cunningjc10@gmail.com>
2018-04-07 22:18:07 -0400

    Favorites refactor

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -6,22 +6,15 @@ class Favorites extends React.Component {
   render() {
     return this.props.favoritesArray.map((favorites) => {
       return (
-        <li className="collection-item friend hack1"  style={{backgroundColor: favorites.color}}>
-          <div className="hack1" style={{backgroundColor: favorites.color}}>
-            <div>
-              <div className="right-align col s12 editbutton"><a className="btn-floating btn-small hackbutton1"><i className="material-icons hackbutton1">mode_edit</i></a></div>
-              <div className="center-align row">
-                <div className="col s4">
+          <div style={{color: favorites.color}}>
+              <div className="row">
+                <div className="col s12">
                   <i className="center-align large material-icons" style={{color: favorites.color}}>insert_chart</i>
-                </div>
-                <div className="col s6">
                   <p>{favorites.publicID}</p>
-                  <a className="waves-effect waves-light btn hackbutton1" style={{color: favorites.color}}>Pay Me</a>
+                  <a className="waves-effect btn-flat" style={{color: favorites.color}}>Pay Me</a>
                 </div>
               </div>
-            </div>
           </div>
-        </li>
       );
     });
   }
