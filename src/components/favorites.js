3f74f68421465105d6c05cd49905e5278e0e6a9a
John Cunningham <cunningjc10@gmail.com>
2018-04-08 01:30:47 -0400

    Willy Boi CSS

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -7,7 +7,7 @@ class Favorites extends React.Component {
     return this.props.favoritesArray.map((favorites) => {
       return (
         <div key={favorites.publicID} className="col s6 friendGroup" style={{color: favorites.color, borderColor: favorites.color}}>
-        	<i className="large material-icons" style={{color: favorites.color}}>hourglass</i>
+        	<i className="large material-icons" style={{color: favorites.color}}>hourglass_empty</i>
         	<p className="truncate publicKey">0xd8F4d1493ec3b76674856b4c01dF4d337B3df97D</p>
         	<a className="waves-effect btn-flat valign-wrapper friendButton">Pay Me</a>
         </div>
