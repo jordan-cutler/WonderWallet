b58527b442e4546436bc398618a5955fd6857dc9
John Cunningham <cunningjc10@gmail.com>
2018-04-07 23:38:40 -0400

    Added ability to fav a contact

--- src/App.js
+++ src/App.js
@@ -267,6 +267,10 @@ class App extends Component {
                   <h5>= $666.69 USD</h5>
                 </div>
                 <div className="col m1"></div>
+                <div className="input-field col s12 finalForm">
+                  <p><b>Hey, Wally here!</b> Choose a color to save this ID!</p>
+                  <input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ffffff" style="width:85%;">
+                </div>
               </div>
             </div>
             <div className="row formRow">
